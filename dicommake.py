3fc6bfda6992236cf9eb55af3e136a270b87a7a3
sandipsamal117@gmail.com <sandipsamal117@gmail.com>
2023-08-03 17:14:17 -0400

    updated memory requirements

--- dicommake.py
+++ dicommake.py
@@ -34,7 +34,7 @@ logger.add(sys.stderr, format=logger_format)
 
 
 
-__version__ = '2.2.2'
+__version__ = '2.2.3'
 
 DISPLAY_TITLE = r"""
        _           _ _                                     _
@@ -354,7 +354,7 @@ def imagePaths_process(*args) -> None:
     parser          = parser,
     title           = 'DICOM image make',
     category        = '',                   # ref. https://chrisstore.co/plugins
-    min_memory_limit= '100Mi',              # supported units: Mi, Gi
+    min_memory_limit= '2Gi',              # supported units: Mi, Gi
     min_cpu_limit   = '1000m',              # millicores, e.g. "1000m" = 1 CPU core
     min_gpu_limit   = 0                     # set min_gpu_limit=1 to enable GPU
 )
